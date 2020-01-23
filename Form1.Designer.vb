1e1c240afa1bc4ee3a433cf5aa82907260d286e6
Samuel Odundo <samieodd@gmail.com>
2020-01-23 20:59:34 +0300

    Created app.

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,95 +22,94 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblFirstName = New System.Windows.Forms.Label()
+        Me.lblSecondName = New System.Windows.Forms.Label()
+        Me.lblLastName = New System.Windows.Forms.Label()
+        Me.lblOutput = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.txtSecondName = New System.Windows.Forms.TextBox()
+        Me.txtThirdName = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
-        'Label1
+        'lblFirstName
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(73, 35)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Label1"
+        Me.lblFirstName.AutoSize = True
+        Me.lblFirstName.Location = New System.Drawing.Point(73, 35)
+        Me.lblFirstName.Name = "lblFirstName"
+        Me.lblFirstName.Size = New System.Drawing.Size(60, 13)
+        Me.lblFirstName.TabIndex = 0
+        Me.lblFirstName.Text = "First Name:"
         '
-        'Label2
+        'lblSecondName
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(76, 77)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(39, 13)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Label2"
+        Me.lblSecondName.AutoSize = True
+        Me.lblSecondName.Location = New System.Drawing.Point(73, 77)
+        Me.lblSecondName.Name = "lblSecondName"
+        Me.lblSecondName.Size = New System.Drawing.Size(72, 13)
+        Me.lblSecondName.TabIndex = 1
+        Me.lblSecondName.Text = "Middle Name:"
         '
-        'Label3
+        'lblLastName
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(76, 118)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(39, 13)
-        Me.Label3.TabIndex = 2
-        Me.Label3.Text = "Label3"
+        Me.lblLastName.AutoSize = True
+        Me.lblLastName.Location = New System.Drawing.Point(73, 118)
+        Me.lblLastName.Name = "lblLastName"
+        Me.lblLastName.Size = New System.Drawing.Size(61, 13)
+        Me.lblLastName.TabIndex = 2
+        Me.lblLastName.Text = "Last Name:"
         '
-        'Label4
+        'lblOutput
         '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(124, 170)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(39, 13)
-        Me.Label4.TabIndex = 3
-        Me.Label4.Text = "Label4"
+        Me.lblOutput.AutoSize = True
+        Me.lblOutput.Location = New System.Drawing.Point(84, 177)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(0, 13)
+        Me.lblOutput.TabIndex = 3
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(152, 261)
+        Me.Button1.Location = New System.Drawing.Point(140, 222)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(119, 23)
         Me.Button1.TabIndex = 4
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "say the thing"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'TextBox1
+        'txtFirstName
         '
-        Me.TextBox1.Location = New System.Drawing.Point(226, 35)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox1.TabIndex = 5
+        Me.txtFirstName.Location = New System.Drawing.Point(226, 35)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(100, 20)
+        Me.txtFirstName.TabIndex = 5
         '
-        'TextBox2
+        'txtSecondName
         '
-        Me.TextBox2.Location = New System.Drawing.Point(226, 77)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox2.TabIndex = 6
+        Me.txtSecondName.Location = New System.Drawing.Point(226, 77)
+        Me.txtSecondName.Name = "txtSecondName"
+        Me.txtSecondName.Size = New System.Drawing.Size(100, 20)
+        Me.txtSecondName.TabIndex = 6
         '
-        'TextBox3
+        'txtThirdName
         '
-        Me.TextBox3.Location = New System.Drawing.Point(226, 118)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox3.TabIndex = 7
+        Me.txtThirdName.Location = New System.Drawing.Point(226, 118)
+        Me.txtThirdName.Name = "txtThirdName"
+        Me.txtThirdName.Size = New System.Drawing.Size(100, 20)
+        Me.txtThirdName.TabIndex = 7
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.TextBox3)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
+        Me.ClientSize = New System.Drawing.Size(413, 287)
+        Me.Controls.Add(Me.txtThirdName)
+        Me.Controls.Add(Me.txtSecondName)
+        Me.Controls.Add(Me.txtFirstName)
         Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.lblLastName)
+        Me.Controls.Add(Me.lblSecondName)
+        Me.Controls.Add(Me.lblFirstName)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -118,12 +117,12 @@ Partial Class Form1
 
     End Sub
 
-    Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Label3 As Label
-    Friend WithEvents Label4 As Label
+    Friend WithEvents lblFirstName As Label
+    Friend WithEvents lblSecondName As Label
+    Friend WithEvents lblLastName As Label
+    Friend WithEvents lblOutput As Label
     Friend WithEvents Button1 As Button
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents txtFirstName As TextBox
+    Friend WithEvents txtSecondName As TextBox
+    Friend WithEvents txtThirdName As TextBox
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,17 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim firstName As String
+        Dim secondName As String
+        Dim thirdName As String
 
+        firstName = txtFirstName.Text
+        secondName = txtSecondName.Text
+        thirdName = txtThirdName.Text
+
+        If txtFirstName.Text = "" Or txtSecondName.Text = "" Or txtThirdName.Text = "" Then
+            lblOutput.Text = "Please input your full name!"
+        ElseIf txtFirstName.Text <> "" And txtSecondName.Text <> "" And txtThirdName.Text <> "" Then
+            lblOutput.Text = "This app was created by " + firstName + " " + secondName + " " + thirdName
+        End If
+    End Sub
 End Class
